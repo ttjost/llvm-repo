7b133f999ae108ef16ecd718929a98b427708ec8
Tiago Trevisan Jost <ttjost@Tiagos-MacBook-Pro.local>
2016-04-21 02:38:53 -0300

    Now Tree Height Reduction is working for matrix 10x10

--- lib/Target/VEX/VEXTreeHeightReduction.cpp
+++ lib/Target/VEX/VEXTreeHeightReduction.cpp
@@ -37,10 +37,12 @@ using namespace llvm;
 namespace {
     
 class VEXTreeHeightReductionPass : public BasicBlockPass {
-    
-    std::map<Instruction *, int> Heights;
-    BasicBlock *basicblock;
-    
+
+    // We have two vectors with the same content. However, we sort them differently
+    // Here we create a vector in order to properly sort the heights of the tree;
+    std::multimap<unsigned, Instruction *> HeightsByLevel;
+    std::map<Instruction *, unsigned> HeightsByInstrOrder;
+
     std::vector<Instruction *> Roots;
     std::deque<Value *> Leaves;
 
@@ -48,9 +50,6 @@ public:
     static char ID;
     
     VEXTreeHeightReductionPass() : BasicBlockPass(ID) {
-        Heights.clear();
-        Roots.clear();
-        Leaves.clear();
     }
     
     const char *getPassName() const override {
@@ -61,7 +60,6 @@ public:
     // sext instructions.
     bool runOnBasicBlock(BasicBlock &BB) override;
     
-    void sortHeights();
     void computeHeights(BasicBlock &BB);
     bool isValidOperation(Instruction* I);
     void computeHeight(Instruction* I);
@@ -77,10 +75,10 @@ public:
     Instruction* climbUp(unsigned Opcode, Instruction *FirstOp, Instruction *SecondOp);
 
     // Huffman Optimization for Tree Height Reduction
-    Instruction *getInstructionUse(Instruction *I);
     void BalanceTree(Instruction *I);
     bool isLeaf(Instruction *I);
-    void getLeaves();
+    void ReplaceAllLeaves();
+    void removeUnrelatedNodes();
     void FindRoots();
     
 };
@@ -103,7 +101,7 @@ bool VEXTreeHeightReductionPass::isValidOperation(Instruction* I) {
 
 void VEXTreeHeightReductionPass::computeHeight(Instruction* I) {
 
-    if (isValidOperation(I) && Heights.find(I) == Heights.end()) {
+    if (isValidOperation(I) && HeightsByInstrOrder.find(I) == HeightsByInstrOrder.end()) {
 
         DEBUG(I->dump());
 
@@ -114,19 +112,20 @@ void VEXTreeHeightReductionPass::computeHeight(Instruction* I) {
         SecondOp = dyn_cast<Instruction>(I->getOperand(1));
 
         if (FirstOp) {
-            if (Heights.find(FirstOp) != Heights.end()) {
-                max = max > Heights[FirstOp] ? max : Heights[FirstOp];
+            if (HeightsByInstrOrder.find(FirstOp) != HeightsByInstrOrder.end()) {
+                max = max > HeightsByInstrOrder[FirstOp] ? max : HeightsByInstrOrder[FirstOp];
             }
         }
 
         if (SecondOp) {
-            if (Heights.find(SecondOp) != Heights.end()) {
-                max = max > Heights[SecondOp] ? max : Heights[SecondOp];
+            if (HeightsByInstrOrder.find(SecondOp) != HeightsByInstrOrder.end()) {
+                max = max > HeightsByInstrOrder[SecondOp] ? max : HeightsByInstrOrder[SecondOp];
             }
         }
-        Heights[I] = 1 + max;
+        HeightsByInstrOrder[I] = 1 + max;
+        HeightsByLevel.insert(std::pair<unsigned, Instruction *>(1 + max, I));
 
-        DEBUG(dbgs() << "Instruction: " << I->getName() << " Height: " << Heights[I] << "\n");
+        DEBUG(dbgs() << "Instruction: " << I->getName() << " Height: " << HeightsByInstrOrder[I] << "\n");
     }
 }
 
@@ -137,192 +136,77 @@ void VEXTreeHeightReductionPass::computeHeights(BasicBlock &BB) {
     }
 }
 
-template <typename T1, typename T2>
-struct less_second {
-    typedef std::pair<T1, T2> type;
-    bool operator ()(type const& a, type const& b) const {
-        return a.second < b.second;
-    }
-};
-
-void VEXTreeHeightReductionPass::sortHeights() {
-
-    // Here we create a vector in order to properly sort the heights of the tree;
-    std::vector<std::pair<Instruction *, int> > sortedHeights;
-
-    for (auto it = Heights.begin(); it != Heights.end(); ++it) {
-        sortedHeights.push_back(*it);
-    }
-    std::sort(sortedHeights.begin(), sortedHeights.end(), less_second<Instruction *, int>());
-
-    // We start over the construction with the correct order
-    Heights.clear();
-
-    for (unsigned i = 0, e = sortedHeights.size(); i != e; ++i) {
-        Heights.insert(sortedHeights[i]);
-    }
-
-}
-
-Instruction* VEXTreeHeightReductionPass::getLaterDefiner(Instruction* I) {
-
-    assert(isa<BinaryOperator>(I) && "Something wrong. Instruction should be BinaryOperator");
-
-    Instruction *FirstOp = cast<Instruction>(I->getOperand(0));
-    Instruction *SecondOp = cast<Instruction>(I->getOperand(1));
-
-    if (Heights[SecondOp] > Heights[FirstOp])
-        return SecondOp;
-    else
-        return FirstOp;
-}
-
-Instruction* VEXTreeHeightReductionPass::getEarlierDefiner(Instruction* I) {
-    
-    assert(isa<BinaryOperator>(I) && "Something wrong. Instruction should be BinaryOperator");
-    
-    Instruction *FirstOp = dyn_cast<Instruction>(I->getOperand(0));
-    Instruction *SecondOp = dyn_cast<Instruction>(I->getOperand(1));
-    
-    if (!FirstOp || (Heights.find(FirstOp) == Heights.end()))
-        return nullptr;
-    
-    if (!SecondOp || (Heights.find(SecondOp) == Heights.end()))
-        return nullptr;
-    
-    if (Heights[SecondOp] < Heights[FirstOp])
-        return SecondOp;
-    else
-        return FirstOp;
-}
-
-/* ************************************************
- Necessary and sufficient conditions for an operation to be hoisted:
-1. One of its definers must be available at least 
-    two cycles earlier than itself on the path selected.
-2. current-op’s later-definer has a definer which is available
-    at least two cycles earlier than current_op’s cycle on that path.
-3. If current-op is ADD or SUB then later-definer has to be either ADD
-    or SUB. (These legal combinations constitute a ‘legal’ chain). If, 
-    on the other hand, current-op is MUL, the later-definer might be 
-    either MUL, ADD or SUB.
-4  Both current-op and its definers have two uses variables.
-5. All relevant nodes on the path (into which new operations
-    are added) have free resources.
-************************************************* */
-
-bool VEXTreeHeightReductionPass::meetsConditions(Instruction *I) {
-    
-    Instruction* LaterDefiner = getLaterDefiner(I);
-    Instruction* EarlierDefiner = getEarlierDefiner(I);
-        
-    if (!LaterDefiner || !EarlierDefiner)
-        return false;
-    
-    // Condition 1
-    if ((Heights[I] - Heights[EarlierDefiner] < 2) &&
-        (Heights[I] - Heights[LaterDefiner] < 2))
-        return false;
-    
-    // Condition2
-    // Isn't condition 2 always satisfied?
-    
-    // Condition 3
-    if (! ((LaterDefiner->getOpcode() == Instruction::Add ||
-        LaterDefiner->getOpcode() == Instruction::Sub) &&
-        (I->getOpcode() == Instruction::Add ||
-        I->getOpcode() == Instruction::Sub))) {
-        return false;
-    }
-    
-    // Condition 4
-    // I don't think this is really necessary.
-    
-    // Condition 5
-    // We don't care about resources.
-    
-    return true;
-}
-
-Instruction *VEXTreeHeightReductionPass::findHighestAvailableOperation(Instruction *I) {
-    for (std::map<Instruction *, int>::iterator it = Heights.begin(); it != Heights.end(); ++it) {
-        if (Heights[it->first] == 1)
-            continue;
-        if (meetsConditions(it->first))
-            return it->first;
-    }
-    return nullptr;
-}
-
-Instruction* VEXTreeHeightReductionPass::climbUp(unsigned Opcode, Instruction *FirstOp, Instruction *SecondOp) {
-    
-    
-    
-}
-
-void VEXTreeHeightReductionPass::associativityAnalysis(Instruction *I) {
-    
-    if (I->getOpcode() == Instruction::Sub)
-        llvm_unreachable("We do not implement subtraction yet. This is not difficult to support, though");
-    
-    Instruction *EarliestOp = findHighestAvailableOperation(I);
-    
-    if (EarliestOp && EarliestOp != I) {
-
-        DEBUG(dbgs() << "Reduce tree height!");
-//        climbUp(I->getOpcode(), getEarlierDefiner(EarliestOp), getEarlierDefiner(I));
-        I->dump();
-        EarliestOp->dump();
-        IRBuilder<> Builder(I);
-        Value* FirstAdd = Builder.CreateAdd(getEarlierDefiner(EarliestOp), getEarlierDefiner(I));
-        EarliestOp->replaceAllUsesWith(FirstAdd);
-        computeHeight(cast<Instruction>(FirstAdd));
-        
-        Value *SecondAdd = Builder.CreateAdd(FirstAdd, getLaterDefiner(EarliestOp));
-        I->replaceAllUsesWith(SecondAdd);
-        computeHeight(cast<Instruction>(SecondAdd));
-        
-//        BB->getInstList().insert(EarliestOp, cast<Instruction>(FirstAdd));
-//        BB->getInstList().insert(I, SecondAdd);
-        
-        EarliestOp->eraseFromParent();
-        I->eraseFromParent();
-        
-        Heights[EarliestOp] = 1;
-        Heights[I] = 1;
-    }
-}
-
 bool VEXTreeHeightReductionPass::isLeaf(Instruction *I) {
 
-    if (Heights[I] == 1)
+    if (HeightsByInstrOrder[I] == 1)
         return true;
     else
         return false;
 }
 
-void VEXTreeHeightReductionPass::getLeaves() {
+void VEXTreeHeightReductionPass::ReplaceAllLeaves() {
 
-    std::map<Instruction *, int>::iterator it = Heights.begin();
-    std::map<Instruction *, int>::iterator itEnd = Heights.end();
+    std::multimap<unsigned, Instruction *>::iterator it = HeightsByLevel.begin();
+    std::multimap<unsigned, Instruction *>::iterator itEnd = HeightsByLevel.end();
+    
+//    Instruction *NewInstr;
+//    IRBuilder<> Builder(HeightsByLevel.rbegin()->second);
+    while (it->first == 1 && it != itEnd) {
+        Instruction *I = it->second;
 
-    while (it->second == 1 && it != itEnd) {
-        Leaves.push_back(it->first);
+//        NewInstr = I->clone();
+//        Builder.Insert(NewInstr);
+//        I->replaceAllUsesWith(NewInstr);
+//        I->eraseFromParent();
+//        Leaves.push_back(cast<Instruction>(NewInstr));
+        Leaves.push_back(cast<Instruction>(I));
         ++it;
     }
 }
 
+void VEXTreeHeightReductionPass::removeUnrelatedNodes() {
+    
+    unsigned LastLevel = HeightsByLevel.rbegin()->first;
+    for (std::multimap<unsigned, Instruction *>::iterator it = HeightsByLevel.begin();
+         it != HeightsByLevel.end();) {
+        
+        bool isPresented = false;
+        
+        if (it->first == LastLevel) {
+            ++it;
+            isPresented = true;
+            continue;
+        }
+        
+        for (Value::use_iterator i = (it->second)->use_begin(),
+             e = (it->second)->use_end(); i != e; ++i) {
+                
+             Instruction *Use = cast<Instruction>(i->getUser());
+             if (HeightsByInstrOrder.find(Use) != HeightsByInstrOrder.end())
+                 isPresented = true;
+        }
+        
+        if (!isPresented) {
+            std::multimap<unsigned, Instruction *>::iterator tempIt = it++;
+            HeightsByLevel.erase(tempIt);
+            HeightsByInstrOrder.erase(tempIt->second);
+        } else
+            ++it;
+    }
+    
+}
+
 void VEXTreeHeightReductionPass::BalanceTree(Instruction *I) {
 
-    std::vector<Value *> WorkList;
+    std::vector<Instruction *> WorkList;
 
     assert(I->getNumOperands() == 2 && "Number of Operands should be 2");
-    WorkList.push_back(I->getOperand(0));
-    WorkList.push_back(I->getOperand(1));
+    WorkList.push_back(dyn_cast<Instruction>(I->getOperand(0)));
+    WorkList.push_back(dyn_cast<Instruction>(I->getOperand(1)));
 
     while (!WorkList.empty()) {
 
-        Instruction *T = cast<Instruction>(WorkList.back());
+        Instruction *T = WorkList.back();
         WorkList.pop_back();
 
         if (std::find(Roots.begin(), Roots.end(), T) != Roots.end())
@@ -330,60 +214,50 @@ void VEXTreeHeightReductionPass::BalanceTree(Instruction *I) {
         else
             if (!isLeaf(I) && T->getOpcode() == I->getOpcode()) {
                 assert(T->getNumOperands() == 2 && "Number of Operands should be 2");
-                WorkList.push_back(T->getOperand(0));
-                WorkList.push_back(T->getOperand(1));
+                WorkList.push_back(dyn_cast<Instruction>(T->getOperand(0)));
+                WorkList.push_back(dyn_cast<Instruction>(T->getOperand(1)));
             }
     }
 
     IRBuilder<> Builder(I);
     Value *R1;
 
-    while (!Leaves.empty()) {
+    while (Leaves.size() > 1) {
         Instruction *Ra1 = dyn_cast<Instruction>(Leaves.front());
         Leaves.pop_front();
         Instruction *Rb1 = cast<Instruction>(Leaves.front());
         Leaves.pop_front();
 
         R1 = Builder.CreateAdd(Ra1, Rb1);
+        
+        R1->dump();
 
         Leaves.push_back(R1);
     }
 
     I->replaceAllUsesWith(R1);
 
-    for (std::map<Instruction *, int>::iterator it = Heights.begin(); it != Heights.end(); ++it) {
-        (it->first)->eraseFromParent();
+    for (std::multimap<unsigned, Instruction *>::reverse_iterator it = HeightsByLevel.rbegin(); it != HeightsByLevel.rend(); ++it) {
+        if (it->first != 1)
+            (it->second)->eraseFromParent();
     }
-}
-
-Instruction *VEXTreeHeightReductionPass::getInstructionUse(Instruction *I) {
-
-    for (std::map<Instruction *, int>::iterator it = Heights.begin(); it != Heights.end(); ++it) {
-        Instruction *InstrUse = it->first;
-        Instruction *Op1 = dyn_cast<Instruction>(InstrUse->getOperand(0));
-        Instruction *Op2 = dyn_cast<Instruction>(InstrUse->getOperand(1));
-
-        if (Op1 == I || Op2 == I) {
-            return InstrUse;
-        }
-    }
-    I->dump();
-    llvm_unreachable("This should never be reached. We must always find a Use of this Instruction within our Tree.");
 }
 
 void VEXTreeHeightReductionPass::FindRoots() {
 
     // We sure must insert the last node as root
-    Roots.push_back(Heights.rbegin()->first);
+    Roots.push_back(HeightsByLevel.rbegin()->second);
 
-    for (std::map<Instruction *, int>::reverse_iterator it = Heights.rbegin(); it != Heights.rend(); ++it) {
+    for (std::map<Instruction *, unsigned>::reverse_iterator it = HeightsByInstrOrder.rbegin(); it != HeightsByInstrOrder.rend(); ++it) {
         Instruction *I = it->first;
 
         if (isLeaf(I))
             continue;
-
-        Instruction* InstrUse = getInstructionUse(I);
-        if ((InstrUse->getOpcode() != I->getOpcode())) {
+        
+        for (Value::use_iterator i = I->use_begin(), e = I->use_end(); i != e; ++i) {
+            
+        }
+        if (I->getNumUses() > 1) {
              llvm_unreachable("This should not be reached by our examples right now. We'll deal with this later.");
         }
     }
@@ -397,32 +271,39 @@ void VEXTreeHeightReductionPass::FindRoots() {
 bool VEXTreeHeightReductionPass::runOnBasicBlock(BasicBlock &BB) {
     
     DEBUG(dbgs() << "VEX Tree Height Reduction!\n");
-    BB.dump();
+    DEBUG(BB.dump());
+    
+    HeightsByLevel.clear();
+    HeightsByInstrOrder.clear();
+    Roots.clear();
+    Leaves.clear();
+    
     computeHeights(BB);
-
-    sortHeights();
-
-    getLeaves();
-
-    if (!Heights.empty()) {
-
+    
+    // There is no way to improve over trees with 3 or less nodes.
+    // We just leave the way it is.
+    if (HeightsByLevel.size() > 3) {
+        
+        removeUnrelatedNodes();
+        ReplaceAllLeaves();
+        
         DEBUG(dbgs() << "All nodes: ");
-        for (std::map<Instruction *, int>::reverse_iterator it = Heights.rbegin(); it != Heights.rend(); ++it) {
-            DEBUG((it->first)->dump());
+        for (std::multimap<unsigned, Instruction *>::reverse_iterator it = HeightsByLevel.rbegin(); it != HeightsByLevel.rend(); ++it) {
+            DEBUG(dbgs() << (it->first) << "\n");
+            DEBUG((it->second)->dump());
         }
-
+        
         DEBUG(dbgs() << "\nLeaves: ");
         for (auto it : Leaves) {
-//            DEBUG(it->dump());
+            DEBUG(it->dump());
         }
-
+        
         FindRoots();
+        
+        BB.dump();
+        return true;
     }
-
-    if (!Heights.empty())
-        FindRoots();
     
-    BB.dump();
     
     return false;
 }
