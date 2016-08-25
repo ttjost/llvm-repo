542e8f0624824c70f3312432831f0fb38c4c07bf
Goncalves <lrgoncalves@drygin3.inf.ufrgs.br>
2016-08-25 09:48:37 -0700

    atualizacao

--- examples/NN/encoder.c
+++ examples/NN/encoder.c
@@ -4,17 +4,13 @@
  * Created on: Sep 9, 2013
  * 			Author: Amir Yazdanbakhsh <a.yazdanbakhsh@gatech.edu>
  */
-//#include "fann.h"
 #include "datatype.h"
 #include "jpegconfig.h"
 #include "prototype.h"
-//#include "time.h"
 #include "rgbimage.h"
 #include "nnconfiguration.h"
-#include <math.h>
-//#include <stdbool.h>
 #include <stdio.h>
-
+#include <math.h>
 
 
 
@@ -37,11 +33,47 @@ UINT16 bitindex = 0;
 INT16 global_ldc1;
 INT16 global_ldc2;
 INT16 global_ldc3;
-
+float it_1[33]; // bias
+	float it_2[64]; // no bias
 	float dataIn [BLOCK_SIZE + 1];
 	float dataOut[BLOCK_SIZE];
-	float layer2[32];
-	float layer3[63];
+	
+
+float poww(float x, float y){
+	int end = (int)y;
+	int i;
+	float result = x;
+	for(i=1;i<end;i++){
+		result = result * x;
+	}
+	return result;
+}
+
+
+
+float fat(long int x){
+
+	int i;
+	float result = 1.0;
+	for(i=0;i<x;i++){
+		result = result * (x-i); 
+	}
+
+	return result;
+}
+
+float expp(float x){
+	int r = 10;
+	int i;
+	float exp1 = 1.0;
+	for(i=1;i<=r;i++){
+		exp1 = exp1 + (poww(x,i)/fat(i));
+	}
+
+
+	return exp1;
+}
+
 
 
 
@@ -58,8 +90,6 @@ int encodeImage(
 	global_ldc3 = 0;
 
 
-
-
 	/** Quantization Table Initialization */
 	initQuantizationTables(qualityFactor);
 
@@ -69,7 +99,7 @@ int encodeImage(
 	/* Writing Marker Data */
 	int k = writeMarkers(outputBuffer, imageFormat, srcImage->w, srcImage->h);
 
-	printf("markers: %d\n",k);
+
 	
 	for (i = 0; i < srcImage->h; i += 8) {
 		for (j = 0; j < srcImage->w; j += 8) {
@@ -77,17 +107,11 @@ int encodeImage(
 			/* Encode the data in MCU */
 			//outputBuffer = encodeMcu(imageFormat, outputBuffer);
 			k = encodeMcu(imageFormat, outputBuffer, k);
-			//printf("encode[%d/%d]: %d\n",i,j,k);
 		}
-		//printf("execution[%d]: %d\n",i,k);
 	}
-	//printf("\n%d\n", k);
-
-	//printf("execution: %d\n",k);
 
 	k = closeBitstream(outputBuffer,k);
 
-	//printf("close bitstream: %d\n",k);
 	return k;
 }
 
@@ -96,7 +120,6 @@ int encodeMcu(UINT32 imageFormat, UINT8 outputBuffer[], int k)
 {
 	int i,j;
 	levelShift(Y1);
-	printf("encodemcu1: %d\n",k);
 
 
 	for (i = 0; i < BLOCK_SIZE; ++i)
@@ -107,56 +130,49 @@ int encodeMcu(UINT32 imageFormat, UINT8 outputBuffer[], int k)
 	//bool isNN = true;
 	
 
-	printf("encodemcu2: %d\n",k);
 
+	
+	dataIn[BLOCK_SIZE] = 1; //bias = 1 
+	int steepness = 1;
 	int q = 0;
 	
-	dataIn[BLOCK_SIZE] = 1;
 
-	for(i = 0; i < layers[1] - 1; i++)
+
+	for(i=0;i<layer_size[1] - 1; i++)
 	{
-		layer2[i] = 0;
-		for(j = 0; j < layers[0]; j++)
+		it_1[i] = 0; 
+		for(j = 0; j < layer_size[0]; j++)
 		{
-			layer2[i] = layer2[i] + dataIn[j] * layer_one[j + q];
+			it_1[i] = it_1[i] + dataIn[j] * weights[j + q];
 		}
-		layer2[i] = layer2[i] * 5.00000000000000000000e-01;
-		//float max_s = 150/5.00000000000000000000e-01;
-		//if (layer2[i] > max_s) layer2[i] = max_s;
-		//else if (layer2[i] < -max_s) layer2[i] = -max_s;
-		layer2[i] = (1.0/(1.0 + exp(-2.0 * (layer2[i] /*+ layer_one[64 + 64 * i]*/))));
+		it_1[i] = it_1[i] * activation[steepness];
+		steepness = steepness + 2;
+		it_1[i] = (1.0/(1.0 + exp(-2.0 * (it_1[i]))));
 		q = q + j;
+	}
+
+	it_1[layer_size[1] - 1] = 1; //bias = 1
 		
-	}
-
-	layer2[layers[1]-1] = 1;	
-
-	printf("encodemcu3: %d\n",k);
-	
-	q = 0;
-	for(i = 0; i < layers[2] - 1; i++)
-	{ 
-		layer3[i] = 0;
-		for(j = 0; j < layers[1]; j++)
-		{//if(j ==layers[1]-1) printf("%f ",layer2[layers[1]-1]);
-			layer3[i] = layer3[i] + layer2[j] * layer_two[j + q]; 
+	for(i=0;i<layer_size[2] - 1; i++)
+	{
+		it_2[i] = 0; 
+		for(j = 0; j < layer_size[1]; j++)
+		{
+			it_2[i] = it_2[i] + it_1[j] * weights[j + q];
 		}
-		layer3[i] = layer3[i] * 5.00000000000000000000e-01;
-		//float max_s = 150/5.00000000000000000000e-01;
-		//if (layer3[i] > max_s) layer3[i] = max_s;
-		//else if (layer3[i] < -max_s) layer3[i] = -max_s;
-		layer3[i] = (2.0/(1.0 + exp(-2.0 * (layer3[i]/* + layer_two[32 + 32* i]*/))) - 1.0f);
+		it_2[i] = it_2[i] * activation[steepness];
+		steepness = steepness + 2;
+		printf("%d,%f\n",i,exp(-2.0 * (it_2[i])));
+		it_2[i] = (2.0/(1.0 + exp(-2.0 * (it_2[i]))) - 1.0f);
 		q = q + j;
 	}
 
-//printf("\nlayer current: %f", layer3[1]);
-
-	for(i = 0; i < layers[2] - 1; i++)
+	for(i = 0; i < layer_size[2] - 1; i++)
 	{	
-		dataOut[i] = layer3[i];
+		dataOut[i] = it_2[i];
+
 	}
 
-	printf("encodemcu4: %d\n",k);
 /*original
 	dct(Y1);
 	quantization(Y1, ILqt);
@@ -173,21 +189,19 @@ int encodeMcu(UINT32 imageFormat, UINT8 outputBuffer[], int k)
 
 	for(i = 0; i < BLOCK_SIZE; ++i)
 	{
-	printf("%f", dataOut[i] * 256.0);	
-	Temp[i] = dataOut[i] * 256.0;
-		printf("Temp[%d] = %d\n", i, Temp[i]);
+		
+		Temp[i] = (INT16)(dataOut[i] * 256.0);
+		
 	}
 	if(1 == 1)
 	{
 		for(i = 8; i < BLOCK_SIZE; ++i)
-
 		{
 			Temp[i] = 0.0;
 		}
 	}
 
-	printf("encodemcu5: %d\n",k);
+
 	k = huffman(1, outputBuffer, k);
-	printf("encodemcu6: %d\n",k);
 	return k;
 }
