68e2841e8e063d0036a1c1263e8eb20578a08bda
Tiago Trevisan Jost <tiagotrevisanjost@gmail.com>
2016-11-14 10:08:43 -0200

    new instructions in HMC

--- examples/pcpowerplus/simple.c
+++ examples/pcpowerplus/simple.c
@@ -1,10 +1,10 @@
-int a[32];
-int b[32];
-int c[32];
+int a[300];
+int b[300];
+int c[300];
 
 int main () {
 
-	for (int i = 0; i < 32; ++i) {
+	for (int i = 0; i < 300; ++i) {
 		c[i] = a[i] + b[i];
 	}
 
